324eeb1259d7e3dad8d69a7f820d4ab3c0f70257
Mahima <mahima@gmail.com>
2016-02-18 10:09:42 +0530

    file changed

--- js/onsubmit.js
+++ js/onsubmit.js
@@ -1,3 +1,13 @@
 document.getElementById("myform").addEventListener("submit", function() {
     alert("form" + " :  " + myform.data.value);
-})
+});
+var x = document.getElementById("name");
+x.addEventListener("select", function() {
+    alert("this is an example of onSelect");
+});
+x.addEventListener("blur",function(){
+    x.value = x.value.toUpperCase();
+});
+x.addEventListener("focus",function(){
+	x.style.backgroundColor = "yellow";
+});
