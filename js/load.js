f6db1b87228babe23185e8b55b7bbe2a314f240e
Mahima <mahima@gmail.com>
2016-02-18 10:42:56 +0530

    changed

--- js/load.js
+++ js/load.js
@@ -1,3 +1,6 @@
 window.addEventListener("load",function(){
 	  alert("This message was triggered from the onload event");
-})
+});
+window.addEventListener("resize", function() {
+    document.getElementById("demo").innerHTML = "abcd";
+});
--- js/resize.js
+++ js/resize.js
@@ -1,3 +1,6 @@
 window.addEventListener("resize", function() {
     document.getElementById("demo").innerHTML = "abcd";
 });
+window.addEventListener("load",function(){
+	  alert("This message was triggered from the onload event");
+});
